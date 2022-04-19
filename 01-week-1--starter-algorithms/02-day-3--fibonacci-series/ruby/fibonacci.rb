2efda2fd46238bf38ed743b4bdfe576f3c153c5a
Eben Eleazer <ebenaeleazer@outlook.com>
2022-04-19 13:42:31 -0400

    potential solution to fibonacci in ruby

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,24 @@
 def fibonacci(num)
   # type your code in here
+  2_prev = 0
+  prev = 1
+  current = 0
+  
+  case num
+    when 0
+      return 0
+    when 1
+      return 1
+    else
+      while i < num
+        current = 2_prev + prev
+        2prev = prev
+        prev = current
+        i++
+      end
+      return current
+    end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -21,3 +40,14 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+
+# get a number
+#zero step is zero
+# first step is 1
+# everyt step after is previous steps added
+
+puts fibonacci(0)
+puts fibonacci(1)
+puts fibonacci(2)
+puts fibonacci(8)
