0f9a3053924cde9adec21ecf7f82a6893facfd5e
Eben Eleazer <ebenaeleazer@outlook.com>
2022-06-22 20:31:12 -0400

    solve shortest string recursive in ruby

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
@@ -1,5 +1,19 @@
 def find_shortest_string_recursive(arr)
   # type your code in here
+  if arr.length == 1
+    return arr.first
+  end
+ 
+  # We are going to narrow our scope of the array with each recursive call
+  # Eventually we will get an array with a length of 1, which will return that element
+  shortest_so_far = find_shortest_string_recursive(arr[1..-1])
+
+  if shortest_so_far <= arr.first
+    return shortest_so_far
+  else
+    return arr.first
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
