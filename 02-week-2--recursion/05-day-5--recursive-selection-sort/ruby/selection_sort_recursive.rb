57455793e8afa4471ac59b01e29c264edcb0b56f
Eben Eleazer <ebenaeleazer@outlook.com>
2022-06-29 20:12:22 -0400

    solve selection sort recursive

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
@@ -1,5 +1,24 @@
 def selection_sort_recursive(arr)
   # type your code in here
+
+  # check to see if array is empty, if so return empty array
+  if arr.empty?
+    return []
+  end
+  # find the minimum value in arr and remove it from the array
+  min = arr.min
+  index_to_remove = arr.index_of(min)
+  arr.delete_at(index_to_remove)
+  
+  # keep track of previous results. Everytime it recurses it will find an remove the smallest value, and add it to the front
+ 
+  results = selection_sort_recursive(arr)
+
+  # slip the smallest value to the front of the array
+   # since recursion operates using a stack, the smallest min will be added last
+
+  result.unshift(min)
+  
 end
 
 if __FILE__ == $PROGRAM_NAME
