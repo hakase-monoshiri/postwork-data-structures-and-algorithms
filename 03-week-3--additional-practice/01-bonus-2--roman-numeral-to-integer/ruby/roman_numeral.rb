8eb5cf64844a54ef53308600289519f15a5a16ef
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-01 23:07:46 -0400

    solve roman numeral in ruby

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
@@ -1,5 +1,44 @@
 def roman_numeral(string)
   # type your code in here
+  running_total = 0
+  prev_numeral_value = 0
+
+  idx = 0
+
+  while idx < string.length
+
+    value = 0
+    numeral = string[idx]
+
+    case numeral
+    when 'I'
+      value = 1
+    when 'V'
+      value = 5
+    when 'X'
+      value = 10
+    when 'L'
+      value = 50
+    when 'C'
+      value = 100
+    when 'D'
+      value = 500
+    when 'M'
+      value = 1000
+    end
+
+    running_total += value
+
+    if prev_numeral_value < value
+      running_total -= (prev_numeral_value * 2)
+    end
+
+    idx += 1
+    prev_numeral_value = value
+  end
+    
+
+  return running_total
 end
 
 if __FILE__ == $PROGRAM_NAME
