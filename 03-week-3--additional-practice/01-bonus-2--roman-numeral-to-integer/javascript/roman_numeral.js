4fc5ad19b9e4f21b334b49e7f9cd87e6d123bf60
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-01 23:19:29 -0400

    solve roman numerals in javascript

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,49 @@
 function romanNumeral(string) {
   // type your code here
+  runningTotal = 0
+  prevNumeralValue = 0
+
+  for (let idx = 0; idx < string.length; idx ++) {
+
+    value = 0
+    numeral = string[idx]
+
+    switch (numeral) {
+    case 'I':
+      value = 1;
+      break;
+    case 'V':
+      value = 5;
+      break;
+    case 'X' :
+      value = 10;
+      break;
+    case 'L':
+      value = 50;
+      break;
+    case 'C' :
+      value = 100;
+      break;
+    case 'D' :
+      value = 500;
+      break;
+    case 'M':
+      value = 1000;
+      break;
+    }
+
+    runningTotal += value
+
+    if ( prevNumeralValue < value ){
+      runningTotal -= (prevNumeralValue * 2)
+    }
+
+    prevNumeralValue = value
+
+  }
+    
+
+  return runningTotal
 }
 
 if (require.main === module) {
