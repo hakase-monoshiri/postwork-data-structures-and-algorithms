45a37da381e1c749f1b7f850fee817e555a1fa02
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-03 21:19:10 -0400

    solve rotate array in ruby

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
@@ -1,5 +1,32 @@
 def rotate_array(arr, k)
   # type your code in here
+
+  shift_value = 0
+   
+  new_arry = []
+  i = 0
+
+  if k > arr.length && arr.length > 0
+    shift_value = k%arr.length
+  else
+    shift_value = k
+  end
+
+  while i < arr.length
+
+    if i + shift_value < arr.length
+    new_idx = i + shift_value
+    else
+    new_idx = (i + shift_value) - arr.length
+    end
+
+    new_arry[new_idx] = arr[i]
+    i += 1
+  end
+
+  return new_arry
+  
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -23,3 +50,11 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# every element in the array has a current index
+# it also has a new index
+# if the current index plus the amount to shift is less than
+#   the array length, that is the nee index
+#   if at amount is bigger than the length, then
+#     the new index is the difference
+
